3fc069587742529072af78e42001e198bce6d4f5
RaidenCXX <volodymyrharash@protonmail.com>
2025-06-02 10:38:17 +0300

    Delete build directory

--- build/CMakeFiles/TheFirst.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for TheFirst.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/TheFirst.dir/source/math.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/TheFirst.dir/source/math.cpp.o: \
- /home/raiden/Projects/TheFirst/source/math.cpp \
- /usr/include/stdc-predef.h /home/raiden/Projects/TheFirst/source/math.h
